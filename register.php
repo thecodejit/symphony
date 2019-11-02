160ad9f1cab7279d6439c5b05a8d1c540597d2ff
Soumyajit <rickdey446@gmail.com>
2019-11-02 23:09:30 +0530

    Add Register file

---
+++ register.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+    <div id="InputContainer">
+        <form id="LoginForm" action="register.php" method="POST">
+            <h1>LOGIN</h1>
+            <p>
+            <label for="LoginUsername">Username</label>
+            <input id="LoginUsername" type="text" placeholder="e.g. Soumya_jit" required>
+            </p>
+            <p>
+            <label for="LoginUsername">Password</label>
+            <input id="LoginPassword" type="password" required>
+</p>
+    <button type="submit">LOG IN</button>
+    </div>
+</body>
+</html>
