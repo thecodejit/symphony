3b4ec07a681792d7fdfef2509b3454c144fc2a45
Soumyajit <rickdey446@gmail.com>
2019-11-03 22:51:34 +0530

    Add User Regustration In Register

--- register.php
+++ register.php
@@ -8,10 +8,53 @@
             <input id="LoginUsername" type="text" placeholder="e.g. Soumya_jit" required>
             </p>
             <p>
-            <label for="LoginUsername">Password</label>
+            <label for="LoginaPassword">Password</label>
             <input id="LoginPassword" type="password" required>
-</p>
+            </p>
     <button type="submit">LOG IN</button>
+    </form>
+
+    <form id="RegisterForm" action="register.php" method="POST">
+            <h1>Register Your Account</h1>
+            <p>
+            <label for="RegisterUsername">Username</label>
+            <input id="RegisterUsername" type="text" placeholder="e.g. Soumya_jit" required>
+            </p>
+
+            <p>
+            <label for="Firstname">Firstname</label>
+            <input id="Firstname" type="text" placeholder="e.g. Soumya" required>
+            </p>
+
+            <p>
+            <label for="Lastname">Lastname</label>
+            <input id="Lastname" type="text" placeholder="e.g. Jit" required>
+            </p>
+
+            <p>
+            <label for="email">email</label>
+            <input id="email" type="text" placeholder="yourname@gmail.com" required>
+            </p>
+
+            <p>
+            <label for="email2">Confirm Email</label>
+            <input id="email2" type="text" placeholder="e.g. Soumya_jit" required>
+            </p>
+
+            <p>
+            <label for="Password">Password</label>
+            <input id="Password" type="password" required>
+            </p>
+
+            <p>
+            <label for="Password2">ConfirmPassword</label>
+            <input id="Password2" type="password" required>
+            </p>
+
+
+    <button type="submit">SIGN UP</button>
+    </form>
+
     </div>
 </body>
 </html>
