d76fa7751e37d1cbb728961ed72687896e0c7b6e
Soumyajit <rickdey446@gmail.com>
2019-11-04 23:45:49 +0530

    Add name variables in register Form

--- register.php
+++ register.php
@@ -1,3 +1,10 @@
+<?php
+    include("include/handler/register-handler.php");
+
+?>
+
+
+
 <html>
 <body>
     <div id="InputContainer">
@@ -18,37 +25,37 @@
             <h1>Register Your Account</h1>
             <p>
             <label for="RegisterUsername">Username</label>
-            <input id="RegisterUsername" type="text" placeholder="e.g. Soumya_jit" required>
+            <input id="RegisterUsername" name ="username" type="text" placeholder="e.g. Soumya_jit" required>
             </p>
 
             <p>
             <label for="Firstname">Firstname</label>
-            <input id="Firstname" type="text" placeholder="e.g. Soumya" required>
+            <input id="Firstname" name ="firstName" type="text" placeholder="e.g. Soumya" required>
             </p>
 
             <p>
             <label for="Lastname">Lastname</label>
-            <input id="Lastname" type="text" placeholder="e.g. Jit" required>
+            <input id="Lastname" name ="LastName" type="text" placeholder="e.g. Jit" required>
             </p>
 
             <p>
             <label for="email">email</label>
-            <input id="email" type="text" placeholder="yourname@gmail.com" required>
+            <input id="email" name="email" type="text" placeholder="yourname@gmail.com" required>
             </p>
 
             <p>
             <label for="email2">Confirm Email</label>
-            <input id="email2" type="text" placeholder="yourname@gmail.com" required>
+            <input id="email2" name="email2" type="text" placeholder="yourname@gmail.com" required>
             </p>
 
             <p>
             <label for="Password">Password</label>
-            <input id="Password" type="password" placeholder="Password Should be 8bit long" required>
+            <input id="Password" name="Password" type="password" placeholder="Password Should be 8bit long" required>
             </p>
 
             <p>
             <label for="Password2">ConfirmPassword</label>
-            <input id="Password2" type="password" placeholder="Your Password" required>
+            <input id="Password2" name="Password2" type="password" placeholder="Your Password" required>
             </p>
 
 
