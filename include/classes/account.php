851bbf69559bce2c55b8375d8206eb7f7b292559
Soumyajit <rickdey446@gmail.com>
2019-11-05 23:09:36 +0530

    Add Validation for Username

---
+++ include/classes/account.php
@@ -0,0 +1,18 @@
+<?php
+    class Account{
+        private $errorArray;
+
+        private function __construct(){
+            $this->errorArray = array();
+        }
+
+        private function validateUsername($un){
+
+            if(strlen($un) > 25 || strlen($un)<5){
+                array_push($this->errorArray, "Username must be between 5 and 25 characters");
+                return;
+            }
+
+        }
+    }
+?>
