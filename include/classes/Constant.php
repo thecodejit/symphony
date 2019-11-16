99fb39d541cdc923a56bea87bfc50e5cb99d6238
Soumyajit Dey <rickdey446@gmail.com>
2019-11-16 23:07:18 +0530

    Add Constant Class

---
+++ include/classes/Constant.php
@@ -0,0 +1,13 @@
+<?php
+class Constant{
+    public static $passworddontmatch = "Your Passwords dont match";
+    public static $passwordnotalphanumeric = "Your Password Should only contain number and letters";
+    public static $passwordcharacters = "Your Password must be between 8 and 25 characters";
+    public static $emailInvalid = "Your Email Is Invalid";
+    public static $emaildontmatch = "Your emails dont match";
+    public static $lastnamecharacter = "your last name must be between 2 and 25 characters";
+    public static $firstnamecharacter = "your first name must be between 2 and 25 characters";
+    public static $usernamecharacters = "Username must be between 5 and 25 characters";
+
+}
+?>
